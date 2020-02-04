2cf0229f340a7f6070112a031b26ccb20966720a
matt bellantoni <mattbellantoni3@gmail.com>
2020-02-04 16:13:11 -0500

    all products

--- gridsome.config.js
+++ gridsome.config.js
@@ -10,6 +10,16 @@ module.exports = {
     "Your one-stop shop for finding all manner of stylish, luxury apparel and accessories",
   plugins: [
     {
+      use: '@gridsome/source-contentful',
+      options: {
+        space: '5jmhrfrccwjk', // required
+        accessToken: 'f2gBvFOj7083vDjZWrsdx24H_-r6FnuNKV3qyO0XR50', // required
+        host: 'cdn.contentful.com',
+        environment: 'master',
+        typeName: 'Contentful'
+      }
+    },
+    {
       use: "@gridsome/source-filesystem",
       options: {
         path: "content/posts/**/*.md",
--- src/pages/new/Products.vue
+++ src/pages/new/Products.vue
@@ -1,8 +1,36 @@
 <template>
   <Layout>
     <h1>Products</h1>
+    <div v-for="edge in $page.allContentfulProduct.edges" :key="edge.node.id">
+      <h2 style="margin-bottom: 0.25em;">{{ edge.node.name }}</h2>
+      <span>${{ edge.node.price }}</span>
+      <q-image :src="edge.node.image.file.url" style="width: 100%; height: 300px; object-fit: contain;" :alt="edge.node.image.title" />
+      <p>{{ edge.node.description }}</p>
+    </div>
   </Layout>
 </template>
+
+<page-query>
+{
+  allContentfulProduct {
+    edges {
+      node {
+        id
+        
+        name
+        price
+        description
+        image {
+          title
+          file {
+            url
+          }
+        }
+      }
+    }
+  }
+}
+</page-query>
 
 <script>
 export default {
